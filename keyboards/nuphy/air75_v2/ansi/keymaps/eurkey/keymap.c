1ffa98853643c795c471e43d21ca6bd190a42a79
Fredrik Nilson <sassur@gmail.com>
2025-12-26 20:52:36 +0100

    Homerow mods på maclagret

--- keyboards/nuphy/air75_v2/ansi/keymaps/eurkey/keymap.c
+++ keyboards/nuphy/air75_v2/ansi/keymaps/eurkey/keymap.c
@@ -543,6 +543,18 @@ tap_dance_action_t tap_dance_actions[] = {
 
 // -------------------- Custom keycode handling --------------------
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case RSFT_T(EU_ODIA):
+            if (record->tap.count && record->event.pressed) {
+                tap_code16(EU_ODIA); // Send EU_ODIA on tap
+                return false;        // Return false to ignore further processing of key
+            }
+            break;
+    }
+    return true;
+}
+
 
 // -------------------- Key Overrides --------------------
 
@@ -576,7 +588,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     SYS_PRT,    KC_INS,     KC_DEL,
     KC_GRV,     TD_1,       TD_2,       TD_3,       TD_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,                 KC_BSPC,    KC_HOME,
     KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       TD_T0,      KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       EU_ARNG,    KC_QUOT,    KC_SLSH,                KC_END,
-    MO(_MACSM), KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       EU_ODIA,    EU_ADIA,                            KC_ENT,     KC_PGUP,
+    MO(_MACSM), HM_A,       HM_S,       HM_D,       HM_F,       KC_G,       KC_H,       HM_J,       HM_K,       HM_L,       HM_ODIA,    EU_ADIA,                            KC_ENT,     KC_PGUP,
     KC_LSFT,    TD_Z,       TD_X,       TD_C,       TD_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_QUES,                            KC_RSFT,    KC_UP,      KC_PGDN,
     KC_LCTL,    KC_LALT,    KC_LGUI,    LT(_MACSM, KC_SPC),                                                                 KC_RALT,    MO(_MACFN), KC_MEH,     KC_LEFT,    KC_DOWN,    KC_RGHT),
 
